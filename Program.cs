4e6484c00bd7f47caa83b5b81524a26213a26372
blackaly <algeserone@yandex.com>
2023-04-24 19:58:29 +0200

    soundit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System; 
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Soundit;
+using System.Net;
+
+class Program{
+    static void Main(string[] args){
+
+        if(args.Length <= 0){
+            Console.WriteLine("SoundIt: ");
+            Console.WriteLine("soundit \t\t [change] <PATH> \t to change directory");
+            return;
+        }
+        Run(args[0]).GetAwaiter().GetResult();
+    }
+
+    static async Task Run(string s){
+
+        SoundIt sound = new SoundIt(s);
+        var data = await sound.Execute();
+        if(string.IsNullOrEmpty(data[1]) || string.IsNullOrEmpty(data[0])){Console.WriteLine("Invalid input!"); return;}        
+        data[1] = RemoveSpecial(data[1]);
+
+        if(string.IsNullOrEmpty(data[1])){
+            data[1] = Path.GetRandomFileName();
+        }
+
+        string PATH = @"C:\Users\"+ Environment.UserName + @"\Downloads\" + $"{data[1]}" + ".mp3";
+        if(File.Exists(PATH)) {
+            Console.WriteLine("File is already EXISTS! Do you need to delete it? [Y/N] "); 
+            string ok = Console.ReadLine();
+            if(!string.IsNullOrEmpty(ok)){
+                ok = ok.ToLower();
+                if(ok.Equals("y")) File.Delete(PATH);
+                else return;
+            }else return; 
+            
+        }
+        try{
+            using(var client = new WebClient()){
+            client.DownloadProgressChanged += DownloadProgressCallback;
+            await client.DownloadFileTaskAsync(data[0], PATH);
+            
+            }
+        }catch(Exception e){
+            Console.WriteLine(e.Message);
+        }
+        Console.WriteLine(PATH);
+    } 
+
+    static void DEBUG(string s){Console.WriteLine(s);}  
+    static void DownloadProgressCallback(object sender, DownloadProgressChangedEventArgs e)
+    {
+        Console.Write($"\rDownloading... {e.ProgressPercentage}% [{new string('#', (int)(e.ProgressPercentage / 10))}{new string('-', (int)(10 - e.ProgressPercentage / 10))}]");
+    }
+
+    static string RemoveSpecial(string str){
+        if(string.IsNullOrEmpty(str)) return string.Empty;
+        string cmp = "#<$+%>!`*'|{?\"}=/:\\@";
+        string res = string.Empty;
+        foreach(var s in str){
+            if(cmp.Contains(s)) continue;
+            res += s; 
+        }
+
+        return res;
+    }
+}
---
+++ SoundIt.cs
@@ -0,0 +1,81 @@
+using System; 
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Text;
+namespace Soundit;
+
+public class SoundIt{
+    private const string PATTERN = @"(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})";
+    private string Youtube; 
+    private string URL; 
+    private Dictionary<string, dynamic> Payload;
+
+    public SoundIt(string url)
+    {
+        this.Payload = new Dictionary<string, dynamic>();
+        this.URL = url;
+        this.Youtube = @"https://www.youtube.com/youtubei/v1/player?key=AIzaSyA8eiZmM1FaDVjRy-df2KTyQ_vz_yYM39w";
+    }
+
+    private async Task<JObject> MakeRequest(string id){
+        var jsonSer = JsonConvert.SerializeObject(this.Payload);
+        var data = new StringContent(jsonSer, Encoding.UTF8, "application/json");
+        using var client = new HttpClient();
+        var response =  await client.PostAsync(this.Youtube, data);
+        var reader = await response.Content.ReadAsByteArrayAsync();
+        var result = Encoding.UTF8.GetString(reader, 0, reader.Length);
+        JObject content = JObject.Parse(result);
+        return content;
+
+    }
+
+    public async Task<string[]> Execute(){
+        string[] elements = new string[2];
+
+        if(string.IsNullOrEmpty(URL)) return elements;
+
+        string id = isCorrectPattern(URL);
+        if(string.IsNullOrEmpty(id)) return elements;
+        AddPayload(id);
+
+        string audio = string.Empty;
+        string videoName = string.Empty;
+        var content = await MakeRequest(id);
+        var res = content["streamingData"]["adaptiveFormats"].ToList();
+        foreach(var r in res){
+            if(r["audioQuality"] != null){
+                string temp = r["audioQuality"].ToString();
+                if(temp.Contains("AUDIO_QUALITY_MEDIUM")){
+                    elements[0] = r["url"].ToString();
+                    elements[1] = content["videoDetails"]["title"].ToString();
+                    break;
+                }
+            }
+        }
+        return elements;
+    }
+
+    private string isCorrectPattern(string s){
+        Match m = Regex.Match(s, PATTERN);
+
+        if(m.Success) return m.Groups[1].Value; 
+        return string.Empty;  
+    }
+
+    private void AddPayload(string id){
+        this.Payload.Add("videoId", id);
+        var context = new Dictionary<string, Dictionary<string, dynamic>>(){
+            {"client", new Dictionary<string, dynamic>(){
+                {"clientName", "ANDROID"},
+                {"clientVersion", "17.10.35"},
+                {"androidSdkVersion", 30}
+            }}
+        };
+        this.Payload.Add("context", context);
+        
+    }
+    
+}
